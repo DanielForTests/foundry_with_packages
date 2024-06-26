69765d6fbe6a65964e3e6614850629e3f93225b4
Daniel PC <raresgeo9@gmail.com>
2024-06-26 15:57:16 +0300

    feat: removed constructor

--- src/Counter.sol
+++ src/Counter.sol
@@ -6,14 +6,6 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Counter {
     uint256 public number;
-    constructor(uint256 _number) {
-        number = _number;
-        if (block.timestamp % 2 == 0) {
-            number = _number;
-        } else {
-            number = 0;
-        }
-    }
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
