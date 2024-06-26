8b96019ffa17320a4f3eb918bd15a2d107e484e5
Daniel PC <raresgeo9@gmail.com>
2024-06-26 21:31:34 +0300

    feat: unsafe code

--- src/Counter.sol
+++ src/Counter.sol
@@ -7,6 +7,15 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract Counter {
     uint256 public number;
 
+    constructor(uint256 _number) {
+        // unsafe use of block timestamp for demonstration purposes
+        if (block.timestamp % 2 == 0) {
+            number = _number + 169;
+        } else {
+            number = _number;
+        }
+    }
+
     function setNumber(uint256 newNumber) public {
         number = newNumber;
     }
