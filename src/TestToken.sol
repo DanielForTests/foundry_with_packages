7cedfd39d35ec5aab2f2b60c396e06b65d9259da
Daniel PC <raresgeo9@gmail.com>
2024-10-08 18:15:38 +0300

    feat: new erc20 token

---
+++ src/TestToken.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TestTokenForDeployment is ERC20 {
+    uint256 public INITIAL_SUPPLY;
+    mapping(address => uint256) private _lastTransferTimestamp;
+
+    constructor() ERC20("TestTokenForDeployment", "TTFD") {
+        INITIAL_SUPPLY = 1000000 * (10 ** uint256(decimals()));
+        _mint(msg.sender, INITIAL_SUPPLY);
+    }
+
+    function _update(
+        address from,
+        address to,
+        uint256 value
+    ) internal override {
+        if (from != address(0) && to != address(0)) {
+            // Exclude minting and burning
+            require(
+                block.timestamp >= _lastTransferTimestamp[from] + 1 weeks,
+                "Tokens are locked for a week after receiving"
+            );
+        }
+
+        _lastTransferTimestamp[to] = block.timestamp;
+
+        super._update(from, to, value);
+    }
+}
